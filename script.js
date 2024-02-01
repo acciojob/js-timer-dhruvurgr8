62acd78f1412b4a78e1a5dc3917ab2feb09895f2
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-01 10:35:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // write js code here if required
+const para = document.getElementById('timer');
+let date =  new Date();
+para.innerText = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
